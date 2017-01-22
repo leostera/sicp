4e5fc06cd52431fd7eb58e1235237adb1b447211
Leandro Ostera <leandro@ostera.io>
2017-01-22 23:42:54 +0100

    Auto-save: Sun Jan 22 23:42:54 CET 2017

--- newton-square-root.rkt
+++ newton-square-root.rkt
@@ -2,10 +2,12 @@
 
 ; newton's square root method
 
+(define (improve guess x)
+  (average guess (/ x guess)))
+
 (define (sqrt guess x)
   (if (good-enough? guess x)
       guess
       (sqrt (improve guess x)
             x)))
-
 
