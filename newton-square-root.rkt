5aab3c83570229d46913314a2b62b8ee2bb58c62
Leandro Ostera <leandro@ostera.io>
2017-01-23 00:06:08 +0100

    Auto-save: Mon Jan 23 00:06:08 CET 2017

--- newton-square-root.rkt
+++ newton-square-root.rkt
@@ -14,12 +14,16 @@
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
 
+; the problem with this is that each of the arguments
+; will be evaluated before being passed in to the `cond`
+; special form, and in the iteration function below
+; (`sqrt-iteration`) that begins an infinite loop
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
         (else else-clause)))
 
 (define (sqrt-iteration guess x)
-  (new-if (good-enough? guess x)
+  (if (good-enough? guess x)
       guess
       (sqrt-iteration (improve guess x)
                    x)))
